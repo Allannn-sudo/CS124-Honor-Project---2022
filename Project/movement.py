7d072184c7542d0fe2dfab0d2c7352186473c510
22justinl <justin7gb@outlook.com>
2022-10-04 18:12:47 -0500

    Finished Gravity Portion
    Finished gravity without other movement mechanics, may need to change if incompatible with movement

--- Project/movement.py
+++ Project/movement.py
@@ -23,48 +23,56 @@ SCREEN_HEIGHT = 600
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.surf = pygame.Surface((75,75))
+        self.surf = pygame.Surface((75,150))
         self.surf.fill((255,255,255))
         # self.surf = pygame.image.load("{IMAGE PATH}").convert()
         # self.surf.set_colorkey((255,255,255), RLEACCEL)
         self.rect = self.surf.get_rect()
+        self.grounded = False
         self.airTime = 0
         self.yVelocity = 0
     def updateYPos(self):
         self.rect.move_ip(0, self.yVelocity)
     #Movement
     # def update(self, pressed_keys):
-        #For jumping, set self.yVelocity to a certain value when button is pressed
+        #For jumping, set self.yVelocity to a certain value when button is pressed?
 
 class Terrain(pygame.sprite.Sprite):
     def __init__(self):
         super(Terrain, self).__init__()
         self.surf = pygame.Surface((150,50))    
-        self.surf.fill((255,124,124))
+        self.surf.fill((0,255,0))
+        #temporary terrain placement (create method with position, shape, and color as argument?)
         self.rect = self.surf.get_rect(
-            center=(
-                0, SCREEN_HEIGHT
+            topleft=(
+                0, SCREEN_HEIGHT - 100
             )
         )
 
 #Initialize Variables?-------
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
-gAccel = -0.2
+gAccel = 0.3
 
 player = Player()
+#temporary
 testTerrain = Terrain()
-
     #Sprite Groups
 all_sprites = pygame.sprite.Group()
+gravity_obj = pygame.sprite.Group()
+terrain = pygame.sprite.Group()
+#add terrain to terrain group when generating terrain using a method?
+
+
+    #Add to groups
 all_sprites.add(player)
 all_sprites.add(testTerrain)
 
-gravity_obj = pygame.sprite.Group()
 gravity_obj.add(player)
-terrain = pygame.sprite.Group()
+
+#temporary
 terrain.add(testTerrain)
-#gravity_obj.add
+
 
 clock = pygame.time.Clock()
 FRAME_RATE = 60
@@ -82,21 +90,26 @@ while running:
 
     #GRAVITY
     for entity in gravity_obj:
-        entity.yVelocity -= gAccel * entity.airTime
-        entity.airTime += 1/30
-
-    #NO GRAVITY IF ON TERRAIN
-    # for entity in pygame.sprite.groupcollide(gravity_obj,terrain,0,0).keys():
-    #     entity.yVelocity = 0
+        if pygame.sprite.spritecollideany(entity, terrain) and entity.grounded == False:
+            entity.grounded = True
+            entity.airtime = 0
+            entity.yVelocity = 0
+            if entity.rect.y < pygame.sprite.spritecollideany(entity, terrain).rect.bottom + 1:
+                entity.rect.y = pygame.sprite.spritecollideany(entity, terrain).rect.top + 1 - entity.rect.h
+        elif pygame.sprite.spritecollideany(entity, terrain) and entity.grounded == True:
+            entity.airtime = 0
+        else:
+            entity.yVelocity += gAccel * entity.airTime
+            entity.airTime += 1/FRAME_RATE
 
 
     screen.fill((0, 0, 0))
     
     for entity in all_sprites:
         screen.blit(entity.surf,entity.rect)
-
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
     player.updateYPos()
-
     pygame.display.flip()
 
     clock.tick(FRAME_RATE)
