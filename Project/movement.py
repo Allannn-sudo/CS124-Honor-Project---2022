b398a04bbc2ee19bef48751b8b206a78fc451ec6
22justinl <justin7gb@outlook.com>
2022-10-03 21:43:20 -0500

    Create movement.py

---
+++ Project/movement.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.locals import (
+    RLEACCEL,
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+
+#Game Loop
+running = False
+while running:
+    #do stuff
